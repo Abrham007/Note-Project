208247c606ebbe377a28c6cc2c83e7cf2985dc75
Abrham007 <abrhamaraya111@gmail.com>
2023-11-11 16:04:01 +0300

    set up the fetch get request for the json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,28 @@ import FlipCard from "./ShowNote/FlipCard";
 
 function App() {
   const [isInput, setIsInput] = useState(true);
+  const [notes, setNotes] = useState([]);
 
   function showNote(event) {
     setIsInput((prevValue) => !prevValue);
     event.preventDefault();
+    fetchNote();
+  }
+
+  async function fetchNote() {
+    try {
+      let response = await fetch("http://localhost:4000");
+      let notesArray = await response.json();
+      setNotes(notesArray);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return isInput ? (
     <InputCard showNote={showNote} />
   ) : (
-    <FlipCard showNote={showNote} />
+    notes.map((note) => <FlipCard note={note} showNote={showNote} />)
   );
 }
 
