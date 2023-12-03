89507991877a4b5889aad4ff4996da65fca8dbaa
Abrham007 <abrhamaraya111@gmail.com>
2023-12-03 16:59:50 +0300

    corrected the bug

--- src/components/ShowNote/MainSlides.jsx
+++ src/components/ShowNote/MainSlides.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 
 function MainSlides(props) {
-  let imgArray = new Uint8Array(props.image);
-  let imgBlob = new Blob([props.image.buffer], { type: "image/png" });
-  const image = URL.createObjectURL(imgBlob);
   return (
     <div className="mySlides">
       <div className="numbertext">1 / 6</div>
-      <img className="main-slides__img" src={image} alt=""></img>
+      <img className="main-slides__img" src={props.image} alt=""></img>
     </div>
   );
 }
