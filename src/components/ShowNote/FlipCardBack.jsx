95d77d7d361a52b0a63b916197c01d9bdff2f534
Abrham007 <abrhamaraya111@gmail.com>
2023-12-04 19:36:00 +0300

    fixed the bug and added the auto functionalty

--- src/components/ShowNote/FlipCardBack.jsx
+++ src/components/ShowNote/FlipCardBack.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from "react";
 import FlipCardImage from "./FlipCardImage";
-import PlayCircleIcon from "@mui/icons-material/PlayCircle";
-import PauseCircleIcon from "@mui/icons-material/PauseCircle";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
+import BackHandIcon from "@mui/icons-material/BackHand";
+import HourglassTopIcon from "@mui/icons-material/HourglassTop";
 
 function FlipCardBack(props) {
   const [noteAudio, setNoteAudio] = useState();
+  const [imgList, setImgList] = useState();
   const [flipCardImage, setFlipCardImage] = useState();
   const [flipCardAudio, setFlipCardAudio] = useState();
+  const [isAuto, setIsAuto] = useState(false);
+  const [duration, setDuration] = useState();
 
   async function fetchMedia() {
     try {
@@ -14,7 +19,6 @@ function FlipCardBack(props) {
         `http://localhost:4000/audio/${props.id}`
       );
       let audioBuffer = await responseAudio.blob();
-      console.log(audioBuffer);
       let audio = URL.createObjectURL(audioBuffer);
       let noteAudio = new Audio(audio);
       setNoteAudio(noteAudio);
@@ -25,17 +29,20 @@ function FlipCardBack(props) {
       );
 
       let responseImg = await fetch(`http://localhost:4000/images/${props.id}`);
-      let imgBufferList = await responseImg.blob();
-      console.log(imgBufferList);
-      let imgURLList = [URL.createObjectURL(imgBufferList)];
-      // let imgURLList = imgBufferList.map((imgBuffer) => {
-      //   let imgBlob = new Blob([imgBuffer.data], { type: "image/png" });
-      //   console.log(imgBlob);
-      //   return URL.createObjectURL(imgBlob);
-      // });
-
-      setFlipCardImage(<FlipCardImage images={imgURLList} />);
-      console.log(imgURLList);
+      let imgBufferList = await responseImg.json();
+      let imgURLList = imgBufferList.map((imgBuffer) => {
+        let imgArray = new Uint8Array(imgBuffer.data);
+        let imgBlob = new Blob([imgArray.buffer], { type: "image/png" });
+        return URL.createObjectURL(imgBlob);
+      });
+      setImgList(imgURLList);
+      setFlipCardImage(
+        <FlipCardImage
+          images={imgURLList}
+          isAuto={isAuto}
+          duration={duration}
+        />
+      );
     } catch (error) {
       console.log(error);
     }
@@ -43,28 +50,63 @@ function FlipCardBack(props) {
 
   function playAudio() {
     noteAudio.play();
+    noteAudio.addEventListener("ended", toggleAuto);
   }
   function pauseAudio() {
     noteAudio.pause();
+  }
+
+  function stopAudio() {
+    noteAudio.pause();
+    noteAudio.currentTime = 0;
+  }
+
+  function toggleAuto() {
+    if (isAuto === false) {
+      setDuration(noteAudio.duration);
+      playAudio();
+      setIsAuto(true);
+    } else {
+      setDuration(0);
+      stopAudio();
+      setIsAuto(false);
+    }
   }
 
   useEffect(() => {
     fetchMedia();
   }, []);
 
+  useEffect(() => {
+    if (imgList) {
+      setFlipCardImage(
+        <FlipCardImage images={imgList} isAuto={isAuto} duration={duration} />
+      );
+    }
+  }, [isAuto]);
+
   return (
     <div className="flip-card-back">
       <div className="flip-card-back__img">{flipCardImage}</div>
       <div className="flip-card-back__container">
         <div className="audio-box">{flipCardAudio}</div>
-        <div className="btn-dropdown">
-          <button className="btn">Note Menu</button>
-          <div className="btn-group">
-            <button className="btn" onClick={(event) => props.showNote(event)}>
-              Create Note
-            </button>
-            <button className="btn">Delete Note</button>
-          </div>
+        <div className="btn-group">
+          <button
+            className="btn--secondary"
+            onClick={(event) => props.showNote(event)}
+          >
+            <AddIcon></AddIcon>
+          </button>
+          <button className="btn--secondary">
+            <DeleteIcon></DeleteIcon>
+          </button>
+          <button className="btn--secondary" onClick={toggleAuto}>
+            {isAuto ? (
+              <HourglassTopIcon></HourglassTopIcon>
+            ) : (
+              <BackHandIcon></BackHandIcon>
+            )}
+          </button>
         </div>
       </div>
     </div>
