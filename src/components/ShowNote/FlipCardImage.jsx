133e14e90e67de8df326edb2df51a85f7bdba1d2
Abrham007 <abrhamaraya111@gmail.com>
2023-12-04 19:36:32 +0300

    added the hand free mode or auto mode

--- src/components/ShowNote/FlipCardImage.jsx
+++ src/components/ShowNote/FlipCardImage.jsx
@@ -4,6 +4,7 @@ import MainSlides from "./MainSlides";
 
 function FlipCardImage(props) {
   const [slideIndex, setSlideIndex] = useState(1);
+  const [autoFuncState, setAutoFuncState] = useState();
   useEffect(() => {
     let i;
     let slides = document.getElementsByClassName("mySlides");
@@ -21,19 +22,53 @@ function FlipCardImage(props) {
 
   function addSlideIndex() {
     setSlideIndex(slideIndex + 1);
+    if (slideIndex === props.images.length) {
+      setSlideIndex(1);
+    }
   }
   function subtractSlideIndex() {
-    setSlideIndex(slideIndex - 1);
+    if (slideIndex !== 1) {
+      setSlideIndex(slideIndex - 1);
+    } else {
+      setSlideIndex(props.images.length);
+    }
   }
 
   function changeIndexTo(n) {
     setSlideIndex(n);
   }
 
+  let autoFunc;
+  useEffect(() => {
+    if (props.isAuto) {
+      let interval = props.duration / props.images.length;
+
+      autoFunc = setInterval(() => {
+        setSlideIndex((preValue) => {
+          if (preValue !== props.images.length) {
+            return preValue + 1;
+          } else {
+            return 1;
+          }
+        });
+      }, interval * 1000);
+      setAutoFuncState(autoFunc);
+    } else {
+      clearInterval(autoFuncState);
+      setAutoFuncState();
+      autoFunc = null;
+    }
+  }, [props.isAuto]);
+
   return (
     <div className="container">
-      {props.images.map((image, index) => (
-        <MainSlides image={image} index={index} key={index} />
+      {props.images.map((image, index, imageList) => (
+        <MainSlides
+          image={image}
+          index={index}
+          imageList={imageList}
+          key={index}
+        />
       ))}
 
       <a className="prev" onClick={subtractSlideIndex} href="#">
@@ -44,10 +79,14 @@ function FlipCardImage(props) {
       </a>
 
       <div className="row">
-        <DemoSlides changeIndexTo={changeIndexTo} />
-        <DemoSlides changeIndexTo={changeIndexTo} />
-        <DemoSlides changeIndexTo={changeIndexTo} />
-        <DemoSlides changeIndexTo={changeIndexTo} />
+        {props.images.map((image, index) => (
+          <DemoSlides
+            changeIndexTo={changeIndexTo}
+            image={image}
+            index={index}
+            key={index}
+          />
+        ))}
       </div>
     </div>
   );
