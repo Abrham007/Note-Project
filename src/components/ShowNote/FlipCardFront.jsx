6d9368d2c5bab73c500bae09a75a3df6119ffb82
Abrham007 <abrhamaraya111@gmail.com>
2023-12-08 20:02:55 +0300

    fixed the bug

--- src/components/ShowNote/FlipCardFront.jsx
+++ src/components/ShowNote/FlipCardFront.jsx
@@ -10,8 +10,8 @@ function FlipCardFront(props) {
           <p>{props.title}</p>
         </div>
         <div>
-          <h2>Date</h2>
-          <p>{props.date}</p>
+          <h2>ID</h2>
+          <p>{props.id}</p>
         </div>
       </div>
       <div className="flip-card-front__question">
